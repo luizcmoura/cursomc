0f43e8a5178ddf1f0fc5f54e7205678761f42ce1
Luiz Moura <luizccmoura@gmail.com>
2018-07-25 14:39:16 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/moura/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.moura.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moura.cursomc.domain.Pedido;
+import com.moura.cursomc.repositories.PedidoRepository;
+import com.moura.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired	private PedidoRepository pedidoRepository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> pedido = pedidoRepository.findById(id);
+		return pedido.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
