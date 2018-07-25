feef9fdde74b003dd13515016bd3908a93aad9dd
Luiz Moura <luizccmoura@gmail.com>
2018-07-25 15:13:07 -0300

    Inserindo uma nova Categoria com Post

--- src/main/java/com/moura/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/moura/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.moura.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.moura.cursomc.domain.Categoria;
 import com.moura.cursomc.services.CategoriaService;
@@ -15,12 +19,19 @@ import com.moura.cursomc.services.CategoriaService;
 public class CategoriaResource {
 	
 	@Autowired
-	private CategoriaService catService;
+	private CategoriaService categoriaService;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 				
-		Categoria cat1 = catService.buscar(id);
+		Categoria cat1 = categoriaService.buscar(id);
 		return ResponseEntity.ok().body(cat1);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert (@RequestBody Categoria categoria){
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/moura/cursomc/services/CategoriaService.java
+++ src/main/java/com/moura/cursomc/services/CategoriaService.java
@@ -19,4 +19,9 @@ public class CategoriaService {
 		return cat.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria categoria) {
+		categoria.setId(null);
+		return categoriaRepository.save(categoria);
+	}
 }
