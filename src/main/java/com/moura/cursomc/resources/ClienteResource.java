af7891be15d753bc09d077dea3c869f694ec37ba
Luiz Moura <luizccmoura@gmail.com>
2018-07-26 14:04:23 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/com/moura/cursomc/resources/ClienteResource.java
+++ src/main/java/com/moura/cursomc/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package com.moura.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.moura.cursomc.domain.Cliente;
+import com.moura.cursomc.dto.ClienteDTO;
 import com.moura.cursomc.services.ClienteService;
 
 @RestController
@@ -22,5 +33,50 @@ public class ClienteResource {
 				
 		Cliente cliente = clienteService.find(id);
 		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert (@Valid @RequestBody ClienteDTO clienteDTO){
+		Cliente cliente = fromDTO(clienteDTO);
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update (@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id){
+		Cliente cliente = fromDTO(clienteDTO);
+		cliente.setId(id);
+		cliente = clienteService.update(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> listCliente = clienteService.findAll();
+		List<ClienteDTO> listClienteDTO = listCliente.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listClienteDTO);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction) {
+		
+		Page<Cliente> pageCliente = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listClienteDTO = pageCliente.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listClienteDTO);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
 	}
 }
--- src/main/java/com/moura/cursomc/services/ClienteService.java
+++ src/main/java/com/moura/cursomc/services/ClienteService.java
@@ -1,12 +1,18 @@
 package com.moura.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.moura.cursomc.domain.Cliente;
 import com.moura.cursomc.repositories.ClienteRepository;
+import com.moura.cursomc.services.exceptions.DataIntegrityException;
 import com.moura.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,5 +24,41 @@ public class ClienteService {
 		Optional<Cliente> cli = clienteRepository.findById(id);
 		return cli.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return clienteRepository.save(cliente);
+	}
+
+	public Cliente update(Cliente clienteOld) {
+		Cliente clienteNew = find(clienteOld.getId());
+		updateData(clienteNew, clienteOld);
+		return clienteRepository.save(clienteNew);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Cliente que já possui entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return clienteRepository.findAll(pageRequest);
+		
+	}
+	
+	private void updateData(Cliente clienteNew, Cliente clienteOld) {
+		clienteNew.setNome(clienteOld.getNome());
+		clienteNew.setEmail(clienteOld.getEmail());
 	}
 }
